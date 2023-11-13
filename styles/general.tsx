54ddf9a713477d0a32d33d526a7439434d6032b8
ali.hamdan <ali.hamdan@wonderdays.com>
2023-11-13 22:06:37 +0400

    jobApp

---
+++ styles/general.tsx
@@ -0,0 +1,10 @@
+import { StyleSheet } from "react-native";
+
+import { COLORS, FONT, SIZES } from "../constants";
+
+export const generalStyles = StyleSheet.create({
+  parentContainer: {
+    width: "100%",
+    paddingHorizontal: SIZES.large,
+  },
+});
